614b444d1eee3882342cfeb406ab123c5e87632e
KensonTsang <kenson.hwtsang@gmail.com>
2026-04-20 02:15:49 +0100

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool validParlindrome(string s, int l, int r) {
+
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        
+        int l = 0;
+        int r = s.size() - 1;
+
+
+        while (l < r) {
+        
+            if (s[l] != s[r]) {
+                return validParlindrome(s, l + 1, r) || validParlindrome(s, l, r - 1);
+            }
+
+            l++;
+            r--;
+        }
+
+
+
+        return true;
+
+    }
+};
