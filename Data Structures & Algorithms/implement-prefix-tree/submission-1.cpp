a6cb01000786344ad6e9230ac133642e2698e11a
KensonTsang <kenson.hwtsang@gmail.com>
2026-06-26 10:40:56 +0100

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,54 @@
+
+struct TrieNode {
+    unordered_map<char, TrieNode*> children;
+    bool isWord = false; 
+};
+
+
+class PrefixTree {
+public:
+    TrieNode* root;
+
+
+    PrefixTree() {        
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        auto* cur = root;
+        for(auto c : word) {
+            if (!cur->children.count(c)) {   // not contain
+                cur->children[c] = new TrieNode();                
+            }            
+            cur = cur->children[c];
+        }
+        cur->isWord = true;
+    }
+    
+    bool search(string word) {
+        auto* cur = root;
+
+        for(auto c : word) {
+            if (!cur->children.count(c)) {
+                return false;
+            }
+            cur = cur->children[c];
+        }
+
+        return cur->isWord;
+    }
+    
+    bool startsWith(string prefix) {
+        auto* cur = root;
+        
+        for (auto c : prefix) {
+            if(!cur->children.count(c)) {
+                return false;
+            }
+            cur = cur->children[c];
+        }
+
+        return true;
+        
+    }
+};
