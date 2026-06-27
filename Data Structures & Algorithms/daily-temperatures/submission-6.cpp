d36c65c092a0e889284136fabe315de09a9a3bbe
KensonTsang <kenson.hwtsang@gmail.com>
2026-06-27 02:10:37 +0100

    Add: daily-temperatures - submission-6

---
+++ Data Structures & Algorithms/daily-temperatures/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+        stack<int> st{};    // minStack
+
+        for (int i = 0 ; i < temperatures.size() ; i++) {
+            while (st.size() > 0 && temperatures[i] > temperatures[st.top()]) {
+                int index = st.top();
+                st.pop();
+                result[index] = i - index;
+            }            
+            st.push(i);
+        }
+
+        return result;
+
+    }
+};
