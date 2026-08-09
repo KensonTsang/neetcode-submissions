0adb1588ec5b95346ef4038f86099743cf01aa7b
KensonTsang <kenson.hwtsang@gmail.com>
2026-08-09 19:54:57 +0100

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        unordered_set<int> mapping{};
+
+        for (const int& num : nums) {
+            mapping.insert(num);            
+        }
+
+
+        int result = 0;
+
+        for (const int& num : mapping) {
+
+            if (mapping.count(num - 1)) {
+                continue;
+            }
+
+            int pushed = 1;
+            while (mapping.count(num + pushed)) {
+                pushed++;                
+            }
+
+            result = max(pushed, result);
+        }
+
+        return result;
+        
+    }
+};
