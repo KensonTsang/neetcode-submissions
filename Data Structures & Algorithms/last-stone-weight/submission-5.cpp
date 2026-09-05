4bd1db0c38a2f5a40014373ab306b6daaad6f4df
KensonTsang <kenson.hwtsang@gmail.com>
2026-09-05 13:32:49 +0100

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq{};
+
+        for (int stone : stones) {
+            pq.push(stone);
+        }
+        
+        while (pq.size() > 1) {
+            int stone1 = pq.top();
+            pq.pop();
+
+            int stone2 = pq.top();
+            pq.pop();
+
+            if (stone1 == stone2) {
+                continue;
+            }            
+
+            if (stone1 > stone2) {
+                pq.push(stone1-stone2);
+            }
+
+            if (stone2 > stone1) {
+                pq.push(stone2-stone1);
+            }
+        }
+        
+        return pq.size() > 0 ? pq.top() : 0;
+    }
+};
